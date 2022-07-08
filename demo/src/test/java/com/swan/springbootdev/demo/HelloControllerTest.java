e16a402314d3264322cba53c6e1c43f9d9b3ee5f
GoldSwan <saltmenu@naver.com>
2022-07-08 18:03:53 +0900

    [ADD]Add hello test code.

--- demo/src/test/java/com/swan/springbootdev/demo/HelloControllerTest.java
+++ demo/src/test/java/com/swan/springbootdev/demo/HelloControllerTest.java
@@ -1,27 +1,42 @@
 package com.swan.springbootdev.demo;
 
-import org.junit.jupiter.api.BeforeEach;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.equalTo;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import com.swan.springbootdev.demo.controller.HelloController;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.http.MediaType;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 public class HelloControllerTest {
     @Autowired
-    private HelloController controller;
-    private MockMvc mock;
-    
-    @BeforeEach
-    public void init() {
-    	mock = MockMvcBuilders.standaloneSetup(controller).build();
-    }
+    private MockMvc mockMvc;
     
     @Test
-    public void hello_return() throws  Exception {
-        String hello = "hello";
-        
+    public void helloGetDefault() throws  Exception {
+        String hello = "Hello World!";
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                                                            .andExpect(status().isOk())
+                                                            .andExpect(content().string(equalTo(hello)));
+    }
+    @Test
+    public void helloGetParam() throws  Exception {
+        String hello = "Hello swan!";
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello?name=swan").accept(MediaType.APPLICATION_JSON))
+                                                            .andExpect(status().isOk())
+                                                            .andExpect(content().string(equalTo(hello)));
+    }
+    @Test
+    public void helloPostParam() throws  Exception {
+        String hello = "Hello World!(Post)";
+        mockMvc.perform(MockMvcRequestBuilders.post("/hello").accept(MediaType.APPLICATION_JSON))
+                                                            .andExpect(status().isOk())
+                                                            .andExpect(content().string(equalTo(hello)));
     }
 }
