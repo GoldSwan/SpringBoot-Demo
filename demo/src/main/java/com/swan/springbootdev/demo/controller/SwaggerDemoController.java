b26002b4f391cbc3e86f68dbe97a9cf0be2d06bf
GoldSwan <saltmenu@naver.com>
2022-07-18 17:06:08 +0900

    [ADD]Add Logger test.

--- demo/src/main/java/com/swan/springbootdev/demo/controller/SwaggerDemoController.java
+++ demo/src/main/java/com/swan/springbootdev/demo/controller/SwaggerDemoController.java
@@ -1,5 +1,7 @@
 package com.swan.springbootdev.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,8 @@ import java.util.*;
 @RequestMapping("/api")
 public class SwaggerDemoController {
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(SwaggerDemoController.class);
+	
 	@GetMapping("board")
 	public Map<String, String> boardSearch(){
 		Map<String, String> res = new HashMap<String, String>();
@@ -17,7 +21,9 @@ public class SwaggerDemoController {
 		res.put("title", "Swagger 사용법");
 		res.put("content", "Swagger 사용법 내용입니다.");
 		res.put("tag", "Swagger");
+		
+		LOGGER.info(res.toString());
+		
 		return res;
 	}
-	
 }
