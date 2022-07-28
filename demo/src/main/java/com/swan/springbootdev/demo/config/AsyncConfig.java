67d8e07006d461b2907b2d4efacc819ee077fc20
GoldSwan <saltmenu@naver.com>
2022-07-28 17:47:36 +0900

    [FIX]Fix Async Config to test.

--- demo/src/main/java/com/swan/springbootdev/demo/config/AsyncConfig.java
+++ demo/src/main/java/com/swan/springbootdev/demo/config/AsyncConfig.java
@@ -14,9 +14,9 @@ public class AsyncConfig extends AsyncConfigurerSupport{
 	@Override
 	public Executor getAsyncExecutor() {
 		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-		executor.setCorePoolSize(4);//기본 실행 대기 중인 Thread 갯수
-		executor.setMaxPoolSize(10);//동시 동작 최대 Thread 갯수
-		executor.setQueueCapacity(100);//MaxPoolSize를 초과하는 요청이 올경우 나중에 처리하기 위해 요청 정보를 임시로 저장해두기 위한 큐 수용량 설정
+		executor.setCorePoolSize(2);//기본 실행 대기 중인 Thread 갯수
+		executor.setMaxPoolSize(5);//동시 동작 최대 Thread 갯수
+		executor.setQueueCapacity(2);//MaxPoolSize를 초과하는 요청이 올경우 나중에 처리하기 위해 요청 정보를 임시로 저장해두기 위한 큐 수용량 설정
 		executor.setThreadNamePrefix("swan-async-");//SpringBoot가 생성하는 스레드 접두사 설정
 		executor.initialize();
 		return executor;
